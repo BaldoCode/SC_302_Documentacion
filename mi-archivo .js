4d88fed8fad47d869bc0cabedf931db5400104bd
Morgan Baldo <145409492+BaldoCode@users.noreply.github.com>
2023-09-18 19:35:41 -0600

    Update mi-archivo .js
    Agregar el link del proyecto y del gist

--- mi-archivo .js
+++ mi-archivo .js
@@ -3,3 +3,6 @@ Alert(“Hola mundo_editado”);
 Console.log (“cambios en el archivo”); 
 
 // Comentario del paso B
+
+// Link Proyecto: https://github.com/users/BaldoCode/projects/1/views/1
+// Link Gist: https://gist.github.com/BaldoCode/d37a1a92e618037a3b27c086cb24c72b
